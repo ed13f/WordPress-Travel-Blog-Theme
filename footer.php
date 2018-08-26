285568b035339eed69f98fa89c898c13482014fa
Enrico De Santis <enricodesantis@Enricos-MBP.hsd1.fl.comcast.net>
2018-08-25 23:27:22 -0400

    apply map styleing

--- footer.php
+++ footer.php
@@ -1,9 +1,12 @@
 <footer>
 	<section class="main-footer">
-		<div>
-			<a target="_blank" href="https://github.com/ed13f"><img alt="Github Logo" src="http://localhost/travel_blog/wp-content/uploads/2018/06/github-icon.png"></a>
-			<a target="_blank" href="https://www.facebook.com/rico.desantis" ><img alt="Facebook Logo" src="http://localhost/travel_blog/wp-content/uploads/2018/06/facebook-icon.png"></a>
-			<a target="_blank" href="https://www.instagram.com/rico_desantis/" ><img alt="Instagram Logo" src="http://localhost/travel_blog/wp-content/uploads/2018/06/black-color-instagram-icon-png.png"></a>
+		<div class="footer-left">
+			<h3>© <?php echo date("Y"); ?> <a href="http://ricodesantis.com" target="_blank" class="footer-link">Rico De Santis Development</a>. All Rights Reserved.</h3>
+		</div>
+		<div class="footer-right">
+			<a target="_blank" href="https://github.com/ed13f"><img id="github-logo" alt="Github Logo" src="http://localhost/travel_blog/wp-content/uploads/2018/06/github-icon.png"></a>
+			<a target="_blank" href="https://www.facebook.com/rico.desantis" ><img id="facebook-logo" alt="Facebook Logo" src="http://localhost/travel_blog/wp-content/uploads/2018/06/facebook-icon.png"></a>
+			<a target="_blank" href="https://www.instagram.com/rico_desantis/" ><img id="instagram-logo" alt="Instagram Logo" src="http://localhost/travel_blog/wp-content/uploads/2018/06/black-color-instagram-icon-png.png"></a>
 		</div>
 	</section>
 </footer>	
