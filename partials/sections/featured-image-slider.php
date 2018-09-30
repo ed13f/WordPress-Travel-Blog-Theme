1bbbe7a182362ca3505d25896fd6cd593e04d7cc
Enrico De Santis <enricodesantis@Enricos-MBP.hsd1.fl.comcast.net>
2018-09-30 11:26:29 -0400

    implement hompage featured slider arrows

--- partials/sections/featured-image-slider.php
+++ partials/sections/featured-image-slider.php
@@ -3,28 +3,30 @@ $counter = 1;
 $wp_query = new WP_Query( $args );
 wp_reset_query();
 $image_count = $wp_query->post_count;
-$arrow_class = " homepage";
+$arrow_class = "scroll-arrow-homepage";
 $slide_arrow = "slider-arrow";
 if($image_count != 0){
 ?>
 
 <section >
-	<?php partial('widgets.destination-scroll-arrows', ['arrow_class' => $arrow_class, "slide_arrow" => $slide_arrow]); ?>
-	<div class="featured-image-slider">
-		<article class="featured-image-slider-container">
-			<?php if ( $wp_query->have_posts() ) : while ( $wp_query->have_posts() ) : $wp_query->the_post(); ?>
-				<div class="slider-image-wrapper <?php echo $counter == 1 ? 'active-img first-image' :''; ?>">
-				<?php the_post_thumbnail();
-				$counter +=1; ?>
-				</div>
-			<?php endwhile; endif; ?>
-			<div class="slider-tracking-container">
-				<?php for ($x = 1; $x <= $image_count; $x++) { ?>
-		   			 <span class="slider-dot <?php echo $x == 1 ? 'active-dot' :''; ?>"><?php echo $x == 1 ? '&#x25A0;' :'&#x25A0;'; ?></span>
-				<?php } ?>
-			</div>	
-		</article>
-	</div>	
+	<div class="featered-slider-wrapper">
+		<?php partial('widgets.destination-scroll-arrows', ['arrow_class' => $arrow_class, "slide_arrow" => $slide_arrow]); ?>
+		<div class="featured-image-slider">
+			<article class="featured-image-slider-container">
+				<?php if ( $wp_query->have_posts() ) : while ( $wp_query->have_posts() ) : $wp_query->the_post(); ?>
+					<div class="slider-image-wrapper <?php echo $counter == 1 ? 'active-img first-image' :''; ?>">
+					<?php the_post_thumbnail();
+					$counter +=1; ?>
+					</div>
+				<?php endwhile; endif; ?>
+				<div class="slider-tracking-container">
+					<?php for ($x = 1; $x <= $image_count; $x++) { ?>
+			   			 <span class="slider-dot <?php echo $x == 1 ? 'active-dot' :''; ?>"><?php echo $x == 1 ? '&#x25A0;' :'&#x25A0;'; ?></span>
+					<?php } ?>
+				</div>	
+			</article>
+		</div>	
+	</div>
 </section>
 
 <?php } ?>
